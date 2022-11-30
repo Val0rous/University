e78771090e471140cf6ac0f0d79edcbb8a509b68
Francesco Valentini <fv26012001@gmail.com>
2022-12-01 00:11:22 +0100

    Added example.php

---
+++ Terzo_Anno/Tecnologie_Web/example.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Example of PHP code
+        </title>
+    </head>
+    <body>
+        <header>
+            I hope this is gonna work!
+        </header>
+        <?php echo "<h1>Vaffanculo!</h1>"; ?>
+    </body>
+</html>
