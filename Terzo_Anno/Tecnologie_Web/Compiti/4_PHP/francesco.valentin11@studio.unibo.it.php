f8bff10c7de8cd108944fcc7bf0aef44837cfdf3
Francesco Valentini <fv26012001@gmail.com>
2022-12-02 00:23:52 +0100

    Completed DB insertion and checks

--- Terzo_Anno/Tecnologie_Web/Compiti/4_PHP/francesco.valentin11@studio.unibo.it.php
+++ Terzo_Anno/Tecnologie_Web/Compiti/4_PHP/francesco.valentin11@studio.unibo.it.php
@@ -7,33 +7,40 @@
     </head>
     <body>
         <?php
-            // Check if variable A is valid
-            function CheckVariableA() {
-                global $A;
+            // Check if variable A is valid and has values in DB
+            function CheckVariableA($A, $conn) {
                 if ($A >= 0 and $A != NULL) {
                     echo "Variable A is valid<br/>";
                 } else {
-                    echo "Variable A is NOT valid!<br/>";
+                    die("Variable A is NOT valid!");
+                }
+                if (CheckSetNotEmpty(1, $conn)) {
+                    echo "Variable A has values in DB<br/>";
+                } else {
+                    die("Variable A has NO values in DB!");
                 }
             }
             
-            // Check if variable B is valid
-            function CheckVariableB() {
-                global $B;
+            // Check if variable B is valid and has values in DB
+            function CheckVariableB($B, $conn) {
                 if ($B >= 0 and $B != NULL) {
                     echo "Variable B is valid<br/>";
                 } else {
-                    echo "Variable B is NOT valid!<br/>";
+                    die("Variable B is NOT valid!");
+                }
+                if (CheckSetNotEmpty(2, $conn)) {
+                    echo "Variable B has values in DB<br/>";
+                } else {
+                    die("Variable B has NO values in DB!");
                 }
             }
             
             // Check if variable O is valid
-            function CheckVariableO() {
-                global $O;
+            function CheckVariableO($O) {
                 if ($O != NULL and ($O === "i" or $O === "u")) {
                     echo "Variable O is valid</br>";
                 } else {
-                    echo "Variable O is NOT valid!<br/>";
+                    die("Variable O is NOT valid!");
                 }
             }
             
@@ -43,9 +50,9 @@
                 $dbuser = "root";
                 $dbpass = "";
                 $dbname = "giugno";
-                $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-                //// or die("Connect failed: %s\n". $conn -> error)
-                ////var_dump($conn);
+                $dbport = 3306;
+                $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname, $dbport);
+
                 if ($conn->connect_error) {
                     die("ERROR: connection to database has failed!</br>" . $conn->connect_error);
                 } else {
@@ -75,7 +82,7 @@
                     $array[$index] = $row;
                     $index++;
                 }
-                //echo '<pre>', var_dump($array), '</pre>';
+
                 return $array;
             }
 
@@ -99,8 +106,8 @@
                 return $newSet;
             }
 
-            // Get next usable index in the database
-            function DBGetNextIndex($conn) {
+            // Get next usable index in the database, useful to insert values in DB
+            function GetNextIndex($conn) {
                 $sql = "SELECT MAX(id) FROM insiemi";
                 $stmt = $conn->prepare($sql);
                 $stmt->execute();
@@ -110,61 +117,62 @@
                 return $index+1;
             }
 
+            // Check if the given set is not empty
+            // If set is not empty, returns true
+            // If set is empty, returns false
+            function CheckSetNotEmpty($set_number, $conn) {
+                $sql = "SELECT COUNT(*) FROM insiemi WHERE insieme=?";
+                $stmt = $conn->prepare($sql);
+                $stmt->bind_param("i", $set_number);
+                $stmt->execute();
+                $stmt->bind_result($num_rows);
+                $stmt->fetch();
 
-            //THEY'RE GONNA BE PASSED VIA A GET FUNCTION LATER ON: YOU'LL LIKELY HAVE TO SEND IT HERE TO LOCALHOST
-            //declaring variables #1 (test)
-            $A = 1;
-            $B = 2;
-            $O = "u";
+                // only returns true if number of rows is valid
+                if ($num_rows > 0) {
+                    return true;
+                }
+                
+                return false;
+            }
+
+            // Inserts values inside DB
+            function InsertValuesInDB($values, $conn) {
+                foreach ($values as $value) {
+                    $index = GetNextIndex($conn);
+                    $sql = "INSERT INTO insiemi (id, valore, insieme) VALUES (?, ?, 3)";
+                    $stmt = $conn->prepare($sql);
+                    $stmt->bind_param("ii", $index, $value);
+                    $stmt->execute();
+                }
+                echo "Values have been correctly inserted into DB";
+            }
 
             $conn = OpenCon();
 
-            CheckVariableA();
-            CheckVariableB();
-            CheckVariableO();
+            // variable data is passed via URL using GET
+            $A = $_GET["A"];
+            $B = $_GET["B"];
+            $O = $_GET["O"];
+
+            CheckVariableA($A, $conn);
+            CheckVariableB($B, $conn);
+            CheckVariableO($O);
 
             $arrayA = ListValuesInSet(1, $conn);
-            //echo '<pre>', var_dump($arrayA), '</pre>';
             $arrayB = ListValuesInSet(2, $conn);
-            //echo '<pre>', var_dump($arrayB), '</pre>';
+            ////echo '<pre>', var_dump($arrayA), '</pre>';
+            ////echo '<pre>', var_dump($arrayB), '</pre>';
 
             $newArray = OperateOnSet($arrayA, $arrayB);
             echo '<pre>', var_dump($newArray), '</pre>';
 
-            ////echo "The next index in database is ".DBGetNextIndex($conn);
+            InsertValuesInDB($newArray, $conn);
+
+            ////echo "The next index in database is ".GetNextIndex($conn);
+            ////echo CheckSetNotEmpty(1, $conn);
             
             CloseCon($conn);
-
-
-
-
-            echo "<br/>SECOND ROUND!<br/><br/>";
-
-
-
-
-            //declaring variables #2 (test)
-            $A = 1;
-            $B = 2;
-            $O = "i";
-
-            $conn = OpenCon();
-
-            CheckVariableA();
-            CheckVariableB();
-            CheckVariableO();
-
-            $arrayA = ListValuesInSet(1, $conn);
-            //echo '<pre>', var_dump($arrayA), '</pre>';
-            $arrayB = ListValuesInSet(2, $conn);
-            //echo '<pre>', var_dump($arrayB), '</pre>';
-
-            $newArray = OperateOnSet($arrayA, $arrayB);
-            echo '<pre>', var_dump($newArray), '</pre>';
-
-            CloseCon($conn);
-
-
         ?>
     </body>
 </html>
