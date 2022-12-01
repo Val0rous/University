1594b4eae22ab28431eda7d79fbfa509c02d5c8f
Francesco Valentini <fv26012001@gmail.com>
2022-12-01 15:21:43 +0100

    Changed PHP homework file

--- Terzo_Anno/Tecnologie_Web/Compiti/4_PHP/francesco.valentin11@studio.unibo.it.php
+++ Terzo_Anno/Tecnologie_Web/Compiti/4_PHP/francesco.valentin11@studio.unibo.it.php
@@ -7,11 +7,6 @@
     </head>
     <body>
         <?php
-            //declaring variables
-            $A = 4;
-            $B;
-            $O = "i"; //it's not a zero, mind that
-
             // Check if variable A is valid
             function CheckVariableA() {
                 global $A;
@@ -21,7 +16,7 @@
                     echo "Variable A is NOT valid!<br/>";
                 }
             }
-
+            
             // Check if variable B is valid
             function CheckVariableB() {
                 global $B;
@@ -31,7 +26,7 @@
                     echo "Variable B is NOT valid!<br/>";
                 }
             }
-
+            
             // Check if variable O is valid
             function CheckVariableO() {
                 global $O;
@@ -41,7 +36,7 @@
                     echo "Variable O is NOT valid!<br/>";
                 }
             }
-
+            
             // Connect to database
             function OpenCon() {
                 $dbhost = "localhost";
@@ -56,7 +51,7 @@
                 } else {
                     echo "ERROR: connection to database has failed!</br>";
                 }
-
+                
                 return $conn;
             }
             
@@ -64,8 +59,50 @@
             function CloseCon() {
                 $conn->close();
             }
+            
+            //THEY'RE GONNA BE PASSED VIA A GET FUNCTION LATER ON: YOU'LL LIKELY HAVE TO SEND IT HERE TO LOCALHOST
+            //declaring variables #1 (test)
+            $A = 1;
+            $B = 2;
+            $O = "u";
 
-            OpenCon();
+            $conn = OpenCon();
+
+            CheckVariableA();
+            CheckVariableB();
+            CheckVariableO();
+
+            //$arrayA;
+            $fillA = $conn->prepare("SELECT valore FROM insiemi WHERE insieme=1");
+            $fillA->execute();
+            //$arrayA = mysqli_fetch_array($resultA, MYSQL_NUM);
+            $resultA = $fillA->get_result();
+
+            var_dump($resultA->fetch_num());
+
+            foreach ($resultA as $item) {
+                static $index = 0;
+                $arrayA[$index] = $resultA->fetch_num();
+                $index++;
+            }
+
+            var_dump($arrayA);
+            
+            //$arrayB;
+            //$fillB = $conn->prepare("SELECT valore FROM tabelle WHERE insieme=2");
+            //$fillA->bind_param("i", $arrayB);
+            //mysql_free_result($resultA);
+            ////var_dump($arrayA);
+            
+
+
+            echo "<br/>Second round!<br/><br/>";
+
+            //declaring variables #2 (test)
+            $A = 1;
+            $B = 2;
+            $O = "i";
+
             CheckVariableA();
             CheckVariableB();
             CheckVariableO();
