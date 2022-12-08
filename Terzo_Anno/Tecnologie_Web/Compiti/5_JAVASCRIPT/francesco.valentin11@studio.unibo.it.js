5e06ebc45d4641c8cc4a8bf042777afe6158a1c8
Francesco Valentini <fv26012001@gmail.com>
2022-12-08 04:32:05 +0100

    Added comment

---
+++ Terzo_Anno/Tecnologie_Web/Compiti/5_JAVASCRIPT/francesco.valentin11@studio.unibo.it.js
@@ -0,0 +1,7 @@
+"use strict";
+
+document.writeln("Hello World!");
+
+// Basically it's an image slider, and the leftmost and rightmost images are useful to 
+// scroll the carousel. There should only be 3 images visible at once, at most, a.k.a.
+// the current image, along the previous and the following ones.
